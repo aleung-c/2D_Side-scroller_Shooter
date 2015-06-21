521482c8afabc53c72ff1c35554c9c62e1d55f38
Antoine JULIEN <ajulien@e1r10p6.42.fr>
2015-06-21 14:03:13 +0200

    pts player

--- Ennemy.hpp
+++ Ennemy.hpp
@@ -6,7 +6,7 @@
 /*   By: ajulien <ajulien@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/06/20 15:31:46 by ajulien           #+#    #+#             */
-/*   Updated: 2015/06/21 13:56:43 by ajulien          ###   ########.fr       */
+/*   Updated: 2015/06/21 14:02:05 by ajulien          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,13 @@
 class Ennemy : public AEntity
 {
 public:
-		Ennemy( void );
 		Ennemy( int x, int y, char c );
 		~Ennemy( void );
 		Ennemy & operator=( Ennemy const &rhs );
 		void	die( void );
 
 private:
-
+	Ennemy( void );
 	void		shoot( void );
 	t_en_obj	*_n;
 	Ennemy( Ennemy const &src );
--- Player.hpp
+++ Player.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Player.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aleung-c <aleung-c@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ajulien <ajulien@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/06/20 11:42:39 by ajulien           #+#    #+#             */
-/*   Updated: 2015/06/20 23:00:48 by aleung-c         ###   ########.fr       */
+/*   Updated: 2015/06/21 14:02:46 by ajulien          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,12 @@ class Player : public AEntity
 {
 	public:
 		Player( int x, int y, char c );
-		Player( Player const &src );
 		~Player( void );
 		Player & operator=( Player const &rhs );
-
 		void	shoot( void );
-		
+
 	private:
+		Player( Player const &src );
 		Player( void );
 };
 
